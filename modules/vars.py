3b7263a32936db3b75c439eb34641f798f376635
Malothuravi188 <malothuravi188@gmail.com>
2024-05-25 23:18:54 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "20047839"
-api_hash = "e635f85a4dae812a26c450c0d41276b0"
-bot_token = "6829337701:AAFH0zySNLs9ti3x8us4Zouzgw72-G50hqo"
+api_id = "21076757"
+api_hash = "cd0e639e8c5b2bd5824e7ac14e2022cb"
+bot_token = "6917453626:AAHhbQ2gm3bUL8VjeVVzbnqVd1S8LQHMyyo"
